1355648b56f956c839994aff769d5803aa5142ad
Martin Woodward <martinwoodward@github.com>
2021-11-11 19:15:20 +0000

    Change color

--- color.py
+++ color.py
@@ -3,18 +3,10 @@
 import time
 from unicornhatmini import UnicornHATMini
 
-print("""Unicorn HAT Mini: colour.py
-
-Display the selected color on the Unicorn HAT Mini's pixels.
-
-Press Ctrl+C to exit!
-
-""")
-
 unicornhatmini = UnicornHATMini()
 unicornhatmini.set_brightness(1)
 while True:
-    # set all pixels to blue
-    unicornhatmini.set_all(0, 0, 255)
+    # set all pixels to green
+    unicornhatmini.set_all(0, 255, 0)
     unicornhatmini.show()
     time.sleep(1.0 / 10)
